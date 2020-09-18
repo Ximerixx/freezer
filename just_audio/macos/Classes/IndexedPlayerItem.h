275840ea4c2bbbeb75819cde3652590465d39a2f
exttex <exttex@protonmail.com>
2020-09-18 19:28:56 +0200

    Pre 0.4.0

--- just_audio/macos/Classes/IndexedPlayerItem.h
+++
@@ -1,9 +0,0 @@
-#import <AVFoundation/AVFoundation.h>
-
-@class IndexedAudioSource;
-
-@interface IndexedPlayerItem : AVPlayerItem
-
-@property (readwrite, nonatomic) IndexedAudioSource *audioSource;
-
-@end
