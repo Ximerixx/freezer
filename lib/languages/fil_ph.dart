f7cbb09bc1828872060062049b3429baf5e767c8
exttex <exttex@protonmail.com>
2020-10-02 20:03:04 +0200

    Language file fix

--- lib/languages/fil_ph.dart
+++ lib/languages/fil_ph.dart
@@ -1,5 +1,6 @@
 /*
 
+Translated by: Chino Pacia
 Revised by: Garri Palao
 
 */
@@ -10,7 +11,7 @@ const language_fil_ph = {
     "Search": "Maghanap",
     "Library": "Library",
     "Offline mode, can't play flow or smart track lists.":
-        "Ikaw ay naka-offline mode, hindi ka pwedeng mag-play ng flow o mga smart track.",
+    "Ikaw ay naka-offline mode, hindi ka pwedeng mag-play ng flow o mga smart track.",
     "Added to library": "Idinagdag na sa library",
     "Download": "I-download",
     "Disk": "Disk",
@@ -29,30 +30,30 @@ const language_fil_ph = {
     "Done": "Tapos na",
     "Delete": "Burahin",
     "Are you sure you want to delete this download?":
-        "Sigurado ka bang buburahin mo ang iyong dinownload?",
+    "Sigurado ka bang buburahin mo ang iyong dinownload?",
     "Cancel": "I-kansel",
     "Downloads": "Mga Download",
     "Clear queue": "I-clear ang queue",
     "This won't delete currently downloading item":
-        "Hindi nito buburahin ang dina-download mo ngayon",
+    "Hindi nito buburahin ang dina-download mo ngayon",
     "Are you sure you want to delete all queued downloads?":
-        "Sigurado ka bang buburahin lahat ang mga dina-download?",
+    "Sigurado ka bang buburahin lahat ang mga dina-download?",
     "Clear downloads history": "I-clear ang kasaysayan ng mga download",
     "WARNING: This will only clear non-offline (external downloads)":
-        "BABALA: Buburahin lang nito ang hindi pang-offline (mga eksternal na download)",
+    "BABALA: Buburahin lang nito ang hindi pang-offline (mga eksternal na download)",
     "Please check your connection and try again later...":
-        "I-check ang iyong koneksiyon at maaaring subukan mo ulit mamaya...",
+    "I-check ang iyong koneksiyon at maaaring subukan mo ulit mamaya...",
     "Show more": "Higit pa",
     "Importer": "Taga-import",
     "Currently supporting only Spotify, with 100 tracks limit":
-        "Suportado lang ang Spotify sa ngayon,na may limit sa 100 mga track",
+    "Suportado lang ang Spotify sa ngayon,na may limit sa 100 mga track",
     "Due to API limitations": "Dahil sa limitasyon ng API",
     "Enter your playlist link below": "Pakilagay ang link ng iyong playlist sa ibaba",
     "Error loading URL!": "Nagkaroon ng problema sa URL!",
     "Convert": "I-convert",
     "Download only": "I-download lang",
     "Downloading is currently stopped, click here to resume.":
-        "Huminto ang download mo, mag-click dito para ituloy",
+    "Huminto ang download mo, mag-click dito para ituloy",
     "Tracks": "Mga Track",
     "Albums": "Mga Album",
     "Artists": "Mga Artist",
@@ -70,27 +71,27 @@ const language_fil_ph = {
     "All offline tracks": "Lahat ng track na pang-offline",
     "Create new playlist": "Gumawa ng bagong playlist",
     "Cannot create playlists in offline mode":
-        "Hindi makagagawa ng playlist habang naka-offline mode",
+    "Hindi makagagawa ng playlist habang naka-offline mode",
     "Error": "Error",
     "Error logging in! Please check your token and internet connection and try again.":
-        "Hindi maka-login! I-check ang iyong token at koneksiyon at ulitin mo.",
+    "Hindi maka-login! I-check ang iyong token at koneksiyon at ulitin mo.",
     "Dismiss": "I-dismiss",
     "Welcome to": "Welcome sa",
     "Please login using your Deezer account.":
-        "Paki-login ang iyong Deezer account",
+    "Paki-login ang iyong Deezer account",
     "Login using browser": "Mag-login gamit ng browser",
     "Login using token": "Mag-login gamit ng token",
     "Enter ARL": "Pakilagay ang ARL",
     "Token (ARL)": "Token (ARL)",
     "Save": "I-save",
     "If you don't have account, you can register on deezer.com for free.":
-        "Kung wala kang account, pumunta sa deezer.com para sa libreng pag-register.",
+    "Kung wala kang account, pumunta sa deezer.com para sa libreng pag-register.",
     "Open in browser": "Buksan sa browser",
     "By using this app, you don't agree with the Deezer ToS":
-        "Sa pag-gamit nitong app, ikaw ay hindi sumusunod sa Deezer ToS",
+    "Sa pag-gamit nitong app, ikaw ay hindi sumusunod sa Deezer ToS",
     "Play next": "I-play ang kasunod",
     "Add to queue": "Idagdag sa queue",
-    "Add track to favorites": Idagdag ang track sa mga paborito",
+    "Add track to favorites": "Idagdag ang track sa mga paborito",
     "Add to playlist": "Idagdag sa playlist",
     "Select playlist": "Piliin ang playlist",
     "Track added to": "Idinagdag ang track sa",
@@ -146,17 +147,17 @@ const language_fil_ph = {
     "External downloads": "Eksternal na download",
     "Content language": "Wika ng nilalaman",
     "Not app language, used in headers. Now":
-        "gagamitin lang ang wika sa header, hindi sa app. Ngayon",
+    "gagamitin lang ang wika sa header, hindi sa app. Ngayon",
     "Select language": "Piliin ang wika",
     "Content country": "Bansa ng nilalaman",
     "Country used in headers. Now": "Gagamitin ang bansa sa mga header. Ngayon",
     "Log tracks": "Log ng mga track",
     "Send track listen logs to Deezer, enable it for features like Flow to work properly":
-        "Ipadala ang log ng mga napakinggang track sa Deezer, I-enable mo para gumana nang maayos sa mga feature kagaya ng Flow",
+    "Ipadala ang log ng mga napakinggang track sa Deezer, I-enable mo para gumana nang maayos sa mga feature kagaya ng Flow",
     "Offline mode": "Offline mode",
     "Will be overwritten on start.": "Papatungan sa simula pa lang.",
     "Error logging in, check your internet connections.":
-        "Hindi maka-login, Pakicheck ang iyong internet connection.",
+    "Hindi maka-login, Pakicheck ang iyong internet connection.",
     "Logging in...": "Nagla-login...",
     "Download path": "Paglalagyan ng download",
     "Downloads naming": "Pagpapangalan sa mga download",
@@ -170,11 +171,11 @@ const language_fil_ph = {
     "Overwrite already downloaded files": "Patungan ang mga nadownload na file",
     "Copy ARL": "Kopyahin ang ARL",
     "Copy userToken/ARL Cookie for use in other apps.":
-        "Kopyahin ang userToken/ARL Cookie para gamitin sa iba pang app.",
+    "Kopyahin ang userToken/ARL Cookie para gamitin sa iba pang app.",
     "Copied": "Nakopya na",
     "Log out": "Mag-Log out",
     "Due to plugin incompatibility, login using browser is unavailable without restart.":
-        "Hindi ka makakapag-login gamit ng browser kung hindi mo ito ire-restart dahil hindi pa compatible ang plugin sa ngayon",
+    "Hindi ka makakapag-login gamit ng browser kung hindi mo ito ire-restart dahil hindi pa compatible ang plugin sa ngayon",
     "(ARL ONLY) Continue": "(ARL LANG) Ituloy",
     "Log out & Exit": "Mag-Log out at Lumabas",
     "Pick-a-Path": "Pumili-ng-Path",
@@ -183,11 +184,9 @@ const language_fil_ph = {
     "Permission denied": "Hindi pinapayagan",
     "Language": "Wika",
     "Language changed, please restart Freezer to apply!":
-        "Pinalitan ang wika, paki-restart ang Deezer para mai-apply!",
+    "Pinalitan ang wika, paki-restart ang Deezer para mai-apply!",
     "Importing...": "Ini-import...",
     "Radio": "Radyo",
-
     "Flow": "Flow",
-
   }
 };
